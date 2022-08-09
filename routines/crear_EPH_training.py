079c6f93dcff13a292186e4e8d7b46aac5832890
matuteiglesias <matuteiglesias@gmail.com>
2022-08-09 19:56:05 -0300

    Update

---
+++ routines/crear_EPH_training.py
@@ -0,0 +1,198 @@
+### PARSEO DE ARGUMENTOS
+
+import argparse
+import pandas as pd
+
+#os.chdir(os.path.dirname(__file__))
+
+parser = argparse.ArgumentParser()
+# parser.add_argument('-i', action='append', nargs='+')
+
+parser.add_argument('-y','--years', nargs='+', help='<Required> Set flag', required=False, type=int, default=[2021, 2022])
+parser.add_argument('-ow','--overwrite', nargs=1, required=False, default= True)
+
+args = parser.parse_args()
+
+overwrite = args.overwrite
+startyr = args.years[0]
+endyr = args.years[1]
+
+# Decision sobre cual es la region de un aglomerado. GBA tiene que ir a Gran Buenos Aires, aunque algunos de sus radios en partidos como Rodriguez, Escobar, etc sean region pampeana.
+# Viedma Patagones, se tendria que tirar de un lado, y la mayoria de sus radios, son Patagonia.
+# Se tiene que corregir a mano, porque el AGLO 0 SI tiene varias regiones.
+AGLO_Region = pd.read_csv('./../data/info/radio_ref.csv', usecols = ['AGLOMERADO', 'Region']).drop_duplicates()
+AGLO_Region = AGLO_Region.loc[~((AGLO_Region.AGLOMERADO == 33) & (AGLO_Region.Region == 'Pampeana'))]
+AGLO_Region = AGLO_Region.loc[~((AGLO_Region.AGLOMERADO == 93) & (AGLO_Region.Region == 'Pampeana'))]
+
+
+####  NOMBRES DE VARIABLES: CENSO - EPH
+names_censo = ['IX_TOT', 'P02', 'P03', 'CONDACT', 'AGLOMERADO',
+    'V01', 'H05', 'H06', 'H07', 'H08', 'H09', 'H10', 'H11', 'H12', 'H16', 'H15', 'PROP', 'H14', 'H13',
+      'P07', 'P08', 'P09', 'P10', 'P05']
+
+names_EPH = ['IX_TOT','CH04','CH06','CONDACT', 'AGLOMERADO',
+    'IV1', 'IV3', 'IV4','IV5','IV6','IV7','IV8','IV10','IV11','II1','II2','II7','II8','II9',
+    'CH09','CH10','CH12','CH13','CH15']
+
+####  NOMBRES DE VARIABLES MONETARIAS
+col_mon = [u'P21', u'P47T', u'PP08D1', u'TOT_P12', u'T_VI', u'V12_M', u'V2_M', u'V3_M', u'V5_M']
+
+####  CARGA DE IPC
+## TRIMESTRAL
+url = 'https://raw.githubusercontent.com/matuteiglesias/IPC-Argentina/main/data/info/indice_precios_Q.csv'
+
+cpi = pd.read_csv(url, index_col = 0)
+cpi.index = pd.to_datetime(cpi.index)
+cpi = cpi['2003':]
+
+## Forzar dia 15 del mes
+cpi.index = cpi.index - pd.offsets.MonthBegin(1) + pd.offsets.Day(14)
+
+## MENSUAL
+url = 'https://raw.githubusercontent.com/matuteiglesias/IPC-Argentina/main/data/info/indice_precios_M.csv'
+cpi_M = pd.read_csv(url, index_col = 0)[['index']]
+cpi_M.index = pd.to_datetime(cpi_M.index)
+cpi_M = cpi_M['2003':]
+
+### INDICE
+ix = cpi_M.loc['2016-01'].values[0][0]
+
+
+## When running the first time we may not have the folder where training data is saved
+import glob
+import os
+
+if not os.path.exists('./../data/training/'):
+    os.makedirs('./../data/training/')
+    
+
+######################################################################
+###########   LOOP PRINCIPAL. CREACION DE TRAINING SETS    ###########
+######################################################################
+
+# from pandas.tseries.offsets import MonthEnd
+
+path ='./../../microdatos-EPH-INDEC/microdatos/' # depende de donde hayamos descargado los microdatos
+# path ='./../../EPH/microdatos/' # depende de donde hayamos descargado los microdatos
+
+for y in range(startyr, endyr):
+    print(y)
+    yr = str(y)[2:]
+    training_file = './../data/training/EPHARG_train_'+str(yr)+'.csv'
+    
+    # Si todavia no existe la training data de ese anio, o si la opcion overwrite esta activada:
+    if (not os.path.exists(training_file)) or (overwrite): 
+
+        allFiles = glob.glob(path + 'hogar/*'+str(yr)+'.txt')
+        frame = pd.DataFrame()
+        list_ = []
+        for file_ in allFiles:
+            df = pd.read_csv(file_,index_col=None, header=0, delimiter = ';',
+                            usecols = ['CODUSU','ANO4','TRIMESTRE','IX_TOT', 'AGLOMERADO',
+            'IV1', 'IV3', 'IV4','IV5','IV6','IV7','IV8','IV10','IV11','II1','II2','II7','II8','II9']) 
+
+            print(len(df))
+            list_ += [df]
+        df = pd.concat(list_)
+
+        # Correcciones Respuestas. Para que matchee censo
+        df = df.loc[df.IV1 != 9]
+        df['IV10'] = df['IV10'].map({1: 1, 2: 2, 3: 2, 0: 0, 9: 9})
+        df['II9'] = df['II9'].map({1: 1, 2: 2, 3: 2, 4: 4, 0: 0})
+        df['II7'] = df['II7'].map({1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 6, 8: 6, 9: 6, 0: 0})
+        df['IX_TOT'] = df['IX_TOT'].clip(0, 8)
+
+        hogar = df
+        hogar = hogar.drop_duplicates()
+        print(hogar.shape)
+
+        allFiles = glob.glob(path + 'individual/usu_individual*'+str(yr)+'.txt')
+        frame = pd.DataFrame()
+        list_ = []
+        for file_ in allFiles:
+            print(file_)
+        #     print(file_)
+            df = pd.read_csv(file_,index_col=None, header=0, delimiter = ';',
+                             usecols = ['CODUSU','ANO4','TRIMESTRE','CH04','CH06', 'AGLOMERADO', 'CH09','CH10','CH12','CH13','CH15'] +\
+                             ['CH07', 'ESTADO','CAT_INAC','CAT_OCUP','PP07G1', 'PP07G2', 'PP07G3', 'PP07G4', 'PP07G_59', 'PP07H', 'PP07I', 'PP07J', 'PP07K',
+                             'P47T', 'V3_M', 'T_VI', 'V12_M', 'TOT_P12', 'V5_M','V2_M', 'PP08D1', 'P21'])
+            df = df.rename(columns = {'ESTADO': 'CONDACT'})
+
+
+            list_ += [df]
+        df = pd.concat(list_)
+
+        # Correcciones Respuestas. Para que matchee censo
+        df['CH15'] = df['CH15'].map({1:1, 2:1, 3:1, 4:2, 5:2, 9:0})
+        df['CH06'] = df['CH06'].clip(0)
+        df['CH09'] = df['CH09'].map({1:1, 2:2, 0:2, 3:2})
+        df.loc[df['CH06'] < 14, 'CONDACT'] = 0 # Menores de 14 van con CONDACT 0, como en el Censo
+
+        ## En Censo, Jardin y educacion especial no preguntan terminado si/no.
+        df['CH12'] = df.CH12.replace(99, 0)
+        df.loc[df.CH12.isin([0, 1, 9]), 'CH13'] = 0
+
+        indiv = df
+        indiv = indiv.dropna(subset = ['P47T'])
+        print(indiv.shape)
+
+        indiv_table = indiv[list(indiv.columns.difference(hogar.columns)) + ['CODUSU', 'ANO4', 'TRIMESTRE', 'AGLOMERADO']]
+        EPH = hogar.merge(indiv_table, on = ['CODUSU', 'ANO4', 'TRIMESTRE', 'AGLOMERADO'])#, indicator = True)
+
+        print('Hogar - Indiv merged:')
+        print(EPH.shape)
+
+        EPH = EPH.merge(AGLO_Region)
+
+        EPH_no_aglo = EPH.copy(); 
+        EPH_no_aglo['AGLOMERADO'] = 0
+
+        EPH = pd.concat([EPH, EPH_no_aglo]).reset_index(drop = True)
+
+        print('No aglo agregado:')
+        print(EPH.shape)
+
+    #     # Quarters / deflation
+
+        # Quarters / deflation
+        EPH['Q'] = EPH.ANO4.astype(str) + ':' + (3*EPH.TRIMESTRE).astype(str)
+        EPH['Q'] = pd.to_datetime(EPH['Q'], format='%Y:%m') - pd.DateOffset(months=1) + pd.DateOffset(days=14)
+        print(EPH['Q'].unique())
+
+
+    #     EPH[col_mon] = cpi_mes_actual*EPH[col_mon].div(EPH[['Q'] + col_mon].merge(cpi, on = 'Q', how = 'left')['index'].values, 0)
+        EPH[col_mon] = ix*EPH[col_mon].div(EPH[['Q'] + col_mon].merge(cpi, on = 'Q', how = 'left')['index'].values, 0)
+        EPH[col_mon] = EPH[col_mon].round()
+
+        print('deflactado:')
+        print(EPH.shape)
+        training = EPH.rename(columns = dict(zip(names_EPH, names_censo)))
+        
+        # remove bad observations
+        training = training.loc[training.P47T >= -0.001].fillna(0)
+        
+        for col in ['CAT_OCUP', 'CH07', 'PP07G1', 'PP07G_59', 'PP07I', 'PP07J', 'PP07K']:
+            training = training.loc[training[col] != 9]
+
+        ### RANKING AGLOMERADO
+        AGLO_rk = training.loc[(training.CAT_OCUP == 3) & (training.P47T >= 100)].groupby(['ANO4', 'AGLOMERADO'])[['P47T']].mean()
+        AGLO_rk['AGLO_rk'] = AGLO_rk.rank(pct = True).round(3)
+        AGLO_rk = AGLO_rk.sort_values('P47T').reset_index()
+
+        ### RANKING REGION
+        Reg_rk = training.loc[(training.CAT_OCUP == 3) & (training.P47T >= 100)].groupby(['ANO4', 'Region'])[['P47T']].mean()
+        Reg_rk['Reg_rk'] = Reg_rk.rank(pct = True).round(3)
+        Reg_rk = Reg_rk.sort_values('P47T').reset_index()
+            
+        training = training.merge(AGLO_rk[['ANO4', 'AGLOMERADO', 'AGLO_rk']]).merge(Reg_rk[['ANO4', 'Region', 'Reg_rk']])
+        
+        ## Crear columnas binarias para ingreso.
+        training['INGRESO'] = (training.P47T > 100).astype(int)
+        training['INGRESO_NLB'] = (training.T_VI > 100).astype(int)
+        training['INGRESO_JUB'] = (training.V2_M > 100).astype(int)
+        training['INGRESO_SBS'] = (training.V5_M > 100).astype(int)
+        
+        ## Ordenar por id de hogar.
+        training = training.sort_values('CODUSU')
+        
+        training.to_csv(training_file, index = False)
---
+++ routines/entrenar_modelos.py
@@ -0,0 +1,121 @@
+### PARSEO DE ARGUMENTOS
+
+import argparse
+import pandas as pd
+
+#os.chdir(os.path.dirname(__file__))
+
+parser = argparse.ArgumentParser()
+# parser.add_argument('-i', action='append', nargs='+')
+
+parser.add_argument('-y','--years', nargs='+', help='<Required> Set flag', required=False, type=int, default=[2021, 2022])
+parser.add_argument('-ow','--overwrite', nargs=1, required=False, default= True)
+
+args = parser.parse_args()
+
+overwrite = args.overwrite
+startyr = args.years[0]
+endyr = args.years[1]
+
+
+import sys
+ipython_vars = ['In', 'Out', 'exit', 'quit', 'get_ipython', 'ipython_vars']
+
+# Column names
+y_cols = ['CAT_OCUP', 'P47T', 'PP10E', 'PP10D', 'PP07K', 'PP07I', 'V3_M', 'PP07G4', 'CH16', 'T_VI', 
+          'V12_M', 'TOT_P12', 'PP07G3', 'V5_M', 'PP07H', 'V2_M', 'PP10C', 
+          'PP08D1', 'PP07J', 'CAT_INAC', 'CH07', 'CH08', 'P21', 'PP07G1', 'PP07G_59', 'PP07G2']
+
+x_cols1 = ['IX_TOT', 'P02', 'P03', 'AGLO_rk', 'Reg_rk', 'V01', 'H05', 'H06',
+       'H07', 'H08', 'H09', 'H10', 'H11', 'H12', 'H16', 'H15', 'PROP', 'H14',
+       'H13', 'P07', 'P08', 'P09', 'P10', 'P05', 'CONDACT']
+
+predecir1 = ['CAT_OCUP', 'CAT_INAC', 'CH07']
+
+x_cols2 = x_cols1 + predecir1
+predecir2 = ['INGRESO', 'INGRESO_NLB', 'INGRESO_JUB', 'INGRESO_SBS']
+
+x_cols3 = x_cols2 + predecir2
+predecir3 = ['PP07G1','PP07G_59', 'PP07I', 'PP07J', 'PP07K']
+
+# Columnas de ingresos. Necesitan una regresion...
+columnas_pesos = [u'P21', u'P47T', u'PP08D1', u'TOT_P12', u'T_VI', u'V12_M', u'V2_M', u'V3_M', u'V5_M']
+
+x_cols4 = x_cols3 + predecir3
+
+predecir4 = columnas_pesos
+y_cols4 = predecir4
+
+
+#####  TRAINING  #####
+
+#####  Funciones clasificador y regresor  #####
+
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+import os
+import joblib
+
+def fit_model(train_data, x_cols, y_cols, out_filename,
+             model):
+    X = train_data[x_cols]
+    y = train_data[y_cols]
+    
+    X, X_test, y, y_test = train_test_split(X, y, test_size=0.1) # less memory used
+    
+    clf = model.fit(X.values, y.values)
+
+    # save the model to disk
+    if not os.path.exists('./../fitted_RF/'):
+        os.makedirs('./../fitted_RF/')
+    joblib.dump(model, out_filename, compress=3)
+    print('saved model at: ' + out_filename)
+
+#     print(sorted([(x, sys.getsizeof(globals().get(x))) for x in dir() if not x.startswith('_') and x not in sys.modules and x not in ipython_vars], key=lambda x: x[1], reverse=True)[:5])
+    del clf
+    del X; del y # liberar memoria eliminando los dataframes mas pesados
+    
+##########################################################################################    
+################ Loop principal. Entrenar y guardar modelos  #############################
+##########################################################################################
+
+import pandas as pd
+from numpy import log10
+
+for yr in [str(s) for s in range(startyr, endyr)]:
+    print(yr)
+    train_data = pd.read_csv('./../data/training/EPHARG_train_'+yr[2:]+'.csv')
+    
+    ## ETAPA 1:
+    out = './../fitted_RF/clf1_'+yr+'_ARG'
+    if (not os.path.exists(out)) or (overwrite):
+        fit_model(train_data, x_cols = x_cols1, y_cols = predecir1, out_filename = out,
+                 model = RandomForestClassifier(n_estimators=100, max_depth = 20, n_jobs = -1))
+    
+    ## ETAPA 2:
+    out = './../fitted_RF/clf2_'+yr+'_ARG'
+    if (not os.path.exists(out)) or (overwrite):
+        fit_model(train_data, x_cols = x_cols2, y_cols = predecir2, out_filename = out,
+                 model = RandomForestClassifier(n_estimators=100, max_depth = 20, n_jobs = -1))
+    
+    ## ETAPA 3:
+    out = './../fitted_RF/clf3_'+yr+'_ARG'
+    if (not os.path.exists(out)) or (overwrite):
+        fit_model(train_data, x_cols = x_cols3, y_cols = predecir3, out_filename = out,
+                 model = RandomForestClassifier(n_estimators=100, max_depth = 20, n_jobs = -1))
+    
+    ## ETAPA 4 (Regresion)
+    ## Tomar log de las columnas en pesos.
+    train_data[columnas_pesos] = log10(train_data[columnas_pesos].clip(-.9) + 1)
+
+    ## Entrenar modelo, para cada trimestre
+    for q in train_data.Q.unique():
+        print(q)
+        out = './../fitted_RF/clf4_'+q+'_ARG'
+        if (not os.path.exists(out)) or (overwrite):
+            train_q = train_data.loc[train_data.Q == q]
+            fit_model(train_q, x_cols = x_cols4, y_cols = y_cols4, out_filename = out,
+                     model = RandomForestRegressor(n_estimators=1, max_depth = 40, n_jobs = -1))
+            del train_q;
+
+    del train_data; 
